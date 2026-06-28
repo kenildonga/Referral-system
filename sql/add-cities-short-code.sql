60906f16d5e90947231f5e2b11a357ca6d863573
Kenil Donga <kenildongakd@gmail.com>
2026-06-28 20:55:00 +0530

    remove unwanted files

--- sql/add-cities-short-code.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE public.cities
-ADD COLUMN IF NOT EXISTS "shortCode" VARCHAR(3);
-
-ALTER TABLE public.cities
-DROP CONSTRAINT IF EXISTS uq_cities_shortCode;
-
-ALTER TABLE public.cities
-ADD CONSTRAINT uq_cities_shortCode UNIQUE ("shortCode");
